a40844205dadf7d14852ac1245e24c7ee1058c3f
Branson Clendenen <bclendenen@csub.edu>
2025-10-08 14:40:49 -0700

    added App, action listeners for MainMainView

--- src/views/MainMenuView.java
+++ src/views/MainMenuView.java
@@ -1,8 +1,9 @@
 package views;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
-public class MainMenuView {
+public class MainMenuView extends JFrame{
     private JPanel mainPanel;
     private JButton wardrobeButton;
     private JButton laundryButton;
@@ -14,5 +15,30 @@ public class MainMenuView {
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+
+    //constructors
+    public MainMenuView(){
+        this.add(mainPanel);
+        this.setSize(400, 400);
+        this.setTitle("Main Menu");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    //action listeners
+    void setWardrobeButtonListener(ActionListener listener){
+        wardrobeButton.addActionListener(listener);
+    }
+    void setLaundryButtonListener(ActionListener listener){
+        laundryButton.addActionListener(listener);
+    }
+    void setPlannerButtonListener(ActionListener listener){
+        plannerButton.addActionListener(listener);
+    }
+    void setStatisticsButtonListener(ActionListener listener){
+        statisticsButton.addActionListener(listener);
+    }
+    void setOutfitGeneratorButtonListener(ActionListener listener){
+        outfitGeneratorButton.addActionListener(listener);
     }
 }
