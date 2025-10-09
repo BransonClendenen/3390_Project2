c44f2568e2722cb9ca60bbfaa669c753bd948288
sam-shahin <sshahin@csub.edu>
2025-10-09 01:25:41 -0700

    statisticsview completed. full OutfitGeneratorView added.

---
+++ src/views/OutfitGeneratorView.java
@@ -0,0 +1,21 @@
+package views;
+
+import javax.swing.*;
+
+public class OutfitGeneratorView {
+    private JPanel mainPanel;
+    private JPanel topPanel;
+    private JButton backButton;
+    private JLabel headerLabel;
+    private JPanel centerPanel;
+    private JTable outfitTable;
+    private JPanel buttonPanel;
+    private JButton generateButton;
+    private JButton finalizeButton;
+    private JPanel contentWrapper;
+    private JPanel backWrapper;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
--- src/views/StatisticsView.java
+++ src/views/StatisticsView.java
@@ -7,13 +7,16 @@ public class StatisticsView {
     private JPanel topPanel;
     private JButton backButton;
     private JPanel togglePanel;
-    private JRadioButton thisMonthRadioButton;
+    private JRadioButton thisMonthButton;
     private JRadioButton threeMonthButton;
     private JPanel centerPanel;
     private JTable statsTable;
     private JPanel bottomPanel;
     private JLabel monthLabel;
-    private ButtonGroup buttonGroup;
+    private JRadioButton allTimeButton;
+    private JPanel radioWrapperPanel;
+    private JPanel backWrapper;
+    private ButtonGroup statsToggleGroup;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
