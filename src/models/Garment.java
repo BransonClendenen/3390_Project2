fad4654b97c43da09de501186a55e432e14754b2
Branson Clendenen <bclendenen@csub.edu>
2025-10-10 14:43:08 -0700

    added note

--- src/models/Garment.java
+++ src/models/Garment.java
@@ -6,6 +6,9 @@ public class Garment {
     private final int garmentID;
     private final String type;
     private final String style;
+    //note for sam: maybe instead of having stats be a seperate data model we could just add it onto garments,
+    //that way the stats are connected to the garment itself and makes it less of a hassle to try to
+    //connect the two seperate models in the controller
 
     public Garment(String name, String color, int garmentID, String type, String style){
         this.name=name;
