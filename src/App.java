34276194d8b7a039f6eb794db09bf9908819171b
Branson Clendenen <bclendenen@csub.edu>
2025-10-10 14:23:33 -0700

    added function calls in App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import models.EventData;
+import models.GarmentData;
+import views.*;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello I am working");
+
+        //create views
+        MainMenuView mainMenuView = new MainMenuView();
+        LaundryView laundryView = new LaundryView();
+        OutfitGeneratorView outfitGeneratorView = new OutfitGeneratorView();
+        PlannerView plannerView = new PlannerView();
+        StatisticsView statisticsView = new StatisticsView();
+        WardrobeView wardrobeView = new WardrobeView();
+
+        //create models
+        GarmentData garmentData = new GarmentData();
+        EventData eventData = new EventData();
     }
 }
